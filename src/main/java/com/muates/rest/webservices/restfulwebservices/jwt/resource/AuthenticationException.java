a0fd5a18b769de131c33bef6fa1140b3253eb971
Murat Ates <muates@ebay.com>
2021-09-04 15:13:41 +0300

    Jwt is added.

---
+++ src/main/java/com/muates/rest/webservices/restfulwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.muates.rest.webservices.restfulwebservices.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
